0ced19fc2af01cbe3be39e420e890e5b37c3ffb1
Robson Pereira <robson.ads.fatec@gmail.com>
2022-01-17 12:31:44 -0300

    Backend project created

---
+++ backend/src/main/java/com/robsonpereira/dscalog/DscalogApplication.java
@@ -0,0 +1,13 @@
+package com.robsonpereira.dscalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/robsonpereira/dscalog/DscalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robsonpereira.dscalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
