ca03f153ab10d9dd6eec911622006a757e4407cd
Robson Pereira <robson.ads.fatec@gmail.com>
2022-01-19 16:05:17 -0300

    Find Category by id, exception handling

--- backend/src/main/java/com/robsonpereira/dscalog/services/CategoryService.java
+++ backend/src/main/java/com/robsonpereira/dscalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package com.robsonpereira.dscalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.robsonpereira.dscalog.dto.CategoryDTO;
 import com.robsonpereira.dscalog.entities.Category;
 import com.robsonpereira.dscalog.repositories.CategoryRepository;
+import com.robsonpereira.dscalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -22,6 +24,13 @@ public class CategoryService {
 		List<Category> list =  repository.findAll();	
 		
 		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+	}
+
+	public CategoryDTO findById(Long id) {
+
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new CategoryDTO(entity);
 	}
 
 }
