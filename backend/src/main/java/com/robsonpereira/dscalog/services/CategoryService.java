2f3c01b78aeee357be7764b9898d9f513b206a5a
Robson Pereira <robson.ads.fatec@gmail.com>
2022-01-21 12:14:40 -0300

    Delete Category

--- backend/src/main/java/com/robsonpereira/dscalog/services/CategoryService.java
+++ backend/src/main/java/com/robsonpereira/dscalog/services/CategoryService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.robsonpereira.dscalog.dto.CategoryDTO;
 import com.robsonpereira.dscalog.entities.Category;
 import com.robsonpereira.dscalog.repositories.CategoryRepository;
+import com.robsonpereira.dscalog.services.exceptions.DatabaseException;
 import com.robsonpereira.dscalog.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -53,6 +56,18 @@ public class CategoryService {
 			return new CategoryDTO(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);					
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Entegrity violation");
 		}
 	}
 }
